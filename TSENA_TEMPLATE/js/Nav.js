6f4b3d3b97edd853af3afbdcafbfc89c89ec181b
Hirintsoa <Hirintsoa@DESKTOP-Q2KQA3F>
2021-07-26 08:48:37 -0700

    Update Nav.js

--- TSENA_TEMPLATE/js/Nav.js
+++ TSENA_TEMPLATE/js/Nav.js
@@ -1,10 +1,10 @@
 function openNav() {
     document.querySelector("nav.sidenav").style.width = "90vw"
-    document.body.style.backgroundColor = "lightgrey"
+    document.body.style.backgroundColor = "darkgrey"
 }
   
 function closeNav() {
     document.querySelector("nav.sidenav").style.width = "0"
     document.querySelector("main").style.marginLeft= "0"
-    document.body.style.backgroundColor = "rgb(56, 54, 54)"
+    document.body.style.backgroundColor = "transparent"
 }
